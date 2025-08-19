8cd43831db09161e0dcb121ecbf757341d72cfec
Lacia7u7 <carlosernestoone@gmail.com>
2025-08-18 21:50:12 -0600

    Added basic assets for cards

--- sequence/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Sequence RL package.
-
-This package implements the core game logic, board layout loading, GUI and
-reinforcement learning components needed to play and train the Sequence board game.
-"""
